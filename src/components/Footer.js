9f8933e62c563259d8b133b564c181064eb7ef54
fliderman01 <fliderman01@gmail.com>
2023-01-04 15:24:06 +0400

    added Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { styled } from "../stitches.config";
+
+const Container = styled("footer", {
+  position: "absolute",
+  bottom: 14,
+  left: "50%",
+  transform: "translate(-50%)",
+  color: "white",
+});
+
+export default function Footer() {
+  return (
+    <Container>
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io/home"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://github.com/BesoBerdzenishvili/Space-Tourism"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Beso Berdzenishvili
+      </a>
+    </Container>
+  );
+}
