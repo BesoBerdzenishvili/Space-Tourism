ba09314a798f36443b1dd8a99c8df469698e2d58
fliderman01 <fliderman01@gmail.com>
2023-01-04 15:09:08 +0400

    added data and stitches configs, plus logo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { globalStyles } from "./stitches.config";
 import "./index.css";
 import App from "./App";
 
+globalStyles();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
---
+++ src/stitches.config.js
@@ -0,0 +1,23 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      black: "#0B0D17",
+      pink: "#D0D6F9",
+    },
+  },
+});
+
+export const globalStyles = globalCss({
+  "*": { boxSizing: "border-box", margin: 0, padding: 0 },
+});
